28f99a6cce97b08d059a4c786e3981d6efae2eb8
katrin-fe <katrinfellner@gmx.net>
2021-04-19 16:02:44 +0200

    changed apiUrl

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ forecastHTML = forecastHTML +
         <div class="col-2">
             <div class="weekday-forecast">${formatDay(forecastDay.dt)}</div>
             <img 
-            src= "http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt=""width="40"/>
+            src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt=""width="40"/>
             <div class="temperature-forecast">
                 <span class="temperature-forecast-max"> ${Math.round(forecastDay.temp.max)}° </span>
                 <span class="temperature-forecast-min"> ${Math.round(forecastDay.temp.min)}° </span>
@@ -69,7 +69,7 @@ temperatureFahrenheit.classList.remove("active");
 function getForecast(coordinates) {
 let apiKey = "535882172e596e21783881f2d1759f05";
 let unit = "metric";
-let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
 axios.get(apiUrl).then(displayForecast);
 }
 
@@ -81,7 +81,7 @@ let windSpeed = document.querySelector("#wind-speed").innerHTML = Math.round(res
 let humidity = document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
 let date = document.querySelector ("#current-day-and-time").innerHTML = formatDate(response.data.dt * 1000);
 let icon = document.querySelector("#symbol");
-icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 getForecast(response.data.coord);
 }
